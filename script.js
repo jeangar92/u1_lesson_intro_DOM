281cb7b2b8a81c95a379caef10a3b826bd39cdd6
Jean Garcia <jeangar92@gmail.com>
2022-02-25 11:44:22 -0500

    my notes

---
+++ script.js
@@ -0,0 +1,36 @@
+// document.querySelector('h1').innerHTML = 'Welcome to the Upside Down';
+
+
+
+// image.style.display = 'none'
+
+// image.style.filter = 'grayscale(100%)'
+// image.style.width =  '95%'
+
+
+// This appends an image inside container of the html file
+// const image = document.querySelector('img');
+
+// const eggos = document.createElement('img');
+
+// eggos.setAttribute('src','https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg')
+
+// document.querySelector('.container').appendChild(eggos)
+
+// eggos.style.width = '50%'
+
+// eggos.remove()
+
+// _______
+
+
+
+
+// in console on chrome :
+
+// document.querySelectorAll('li')[0].innerText = 'Demo Dog Hangout'
+// 'Demo Dog Hangout'
+// document.querySelectorAll('li')[4].style.color = 'red'
+// 'red'
+// document.querySelectorAll('li')[3].remove()
+// undefined
